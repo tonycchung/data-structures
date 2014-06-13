8b306b0d01dd1b6e6c576a5192d790ad6aa66224
Tony Chung <anthonyuchung@gmail.com>
2014-06-13 01:50:34 -0700

    Add radix sort

--- test/sorts/sort_test_helper.rb
+++ test/sorts/sort_test_helper.rb
@@ -1,5 +1,5 @@
 def make_rand_list
-  @rand_list = (0..1000).map{ rand(1000) }
+  @rand_list = (0..10000).map{ rand(-1000..1000) }
   @sorted_list = @rand_list.sort
   @reverse_list = @rand_list.sort.reverse
 end
